3d11a225bc2e66bf694d48a894c9d829e26b0180
Yaying <yliangli20@stuy.edu>
2021-02-04 15:36:35 -0500

    started working on printNoDoubleLetterWords method

--- MakeWords.java
+++ MakeWords.java
@@ -1,34 +1,5 @@
 public class MakeWords {
 
-  // //wrapper method
-  // public static void printBinary(int length){
-  //   printBinary(length,"");
-  // }
-  // public static void printBinary(int n,String word){
-  //   if(n == word.length()){
-  //     System.out.println(word);
-  //   }else{
-  //     printBinary(n,word+"0");
-  //     printBinary(n,word+"1");
-  //   }
-  // }
-
-  /*
-@param n : the length of the words to print
-@param letters : an array of all valid characters
-@param word: partial solution
-print all words made of n letters
-*/
-// public static void printWords(int n,String word,char[]letters){
-//   if(n == 0){
-//     System.out.println(word);
-//   }else{
-//     for ( char c : letters){
-//       printWords(n-1,word+c,letters);
-//     }
-//   }
-// }
-
   /*Print all words that are made of the letters a-z inclusive.
   *@param length : the length of the words that are to be printed
   */
@@ -37,11 +8,10 @@ print all words made of n letters
   }
 
   /*Print all words that are made of the letters a-z inclusive.
-  *@param length : either how many more letters need to be
   *@param word : the partial word so far.
   */
   public static void printAllWords(int length,String word){
-    if (word.length() == length) { //int length ain't changing
+    if (word.length() == length) { //int length stays the same
       System.out.println(word);
     }
     else { //using ASCII
@@ -51,10 +21,31 @@ print all words made of n letters
     }
   }
 
+  /*Print all words that are made of the characters in letters. There may not be consecutive equal letters,
+  *aax is not allowed, but axa is allowed.
+  *@param length : the length of the words that are to be printed
+  *@param letters: the letters you should be using
+  */
+  public static void printNoDoubleLetterWords(int length,char[] letters){
+    printNoDoubleLetterWords(length,"",letters);
+  }
+
+  /*Print all words that are made of the characters in letters. There may not be consecutive equal letters,
+  *aax is not allowed, but axa is allowed.
+  *@param length : either how many more letters need to be
+  *@param word   : the partial word so far.
+  *@param letters: the letters you should be using
+  */
+  public static void printNoDoubleLetterWords(int length,String word,char[]letters){
+    if (word.length() == length) {
+      System.out.println(word);
+    }
+    else {
+      
+    }
+  }
+
   public static void main(String[] args) {
-    //printBinary(3);
-    // char[] lotsaChars = new char[]{'a','b','c','d','e'};
-    // printWords(3, "", lotsaChars);
     printAllWords(3);
   }
 
